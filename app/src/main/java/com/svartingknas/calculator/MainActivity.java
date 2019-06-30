1da5a9831325406422ed3bcc1a34b28a5430aec0
svartingknas <svartingknas@gmail.com>
2019-06-30 14:41:26 -0600

    bug fix / orientation issue

--- app/src/main/java/com/svartingknas/calculator/MainActivity.java
+++ app/src/main/java/com/svartingknas/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.svartingknas.calculator;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,8 +15,10 @@ public class MainActivity extends AppCompatActivity {
 
     //Variables to hold the operands and type of calculations:
     private Double operand1 = null;
-    private Double operand2 = null;
     private String pendingOperation = "=";
+
+    private static final String STATE_PENDING_OPERATION = "PendingOperation";
+    private static final String STATE_OPERAND1 = "Operand1";
 
 
     @Override
@@ -71,46 +74,70 @@ public class MainActivity extends AppCompatActivity {
                 Button b = (Button) v;
                 String op = b.getText().toString();
                 String value = newNumber.getText().toString();
-                if (value.length() != 0){
-                    performOperation(value, op);
+                try {
+                    Double doubleValue = Double.valueOf(value);
+                    performOperation(doubleValue, op);
+                } catch (NumberFormatException e) {
+                    newNumber.setText("");
                 }
                 pendingOperation = op;
                 displayOperation.setText(pendingOperation);
             }
         };
+
         buttonEquals.setOnClickListener(opListener);
         buttonDivide.setOnClickListener(opListener);
         buttonMultiply.setOnClickListener(opListener);
         buttonMinus.setOnClickListener(opListener);
         buttonPlus.setOnClickListener(opListener);
     }
-    private void performOperation(String value, String operation){
-        if (null == operand1){
-            operand1 = Double.valueOf(value);
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation);
+        if (operand1 != null){
+            outState.putDouble(STATE_OPERAND1, operand1);
+        super.onSaveInstanceState(outState);
+    }
+
+}
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+         pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION);
+         operand1 = savedInstanceState.getDouble(STATE_OPERAND1);
+         displayOperation.setText(pendingOperation);
+
+    }
+
+    private void performOperation(Double value, String operation) {
+
+        if (null == operand1) {
+            operand1 = value;
         } else {
-            operand2 = Double.valueOf(value);
-            if (pendingOperation.equals("=")){
+            if (pendingOperation.equals("=")) {
                 pendingOperation = operation;
             }
-            switch (pendingOperation){
+            switch (pendingOperation) {
                 case "=":
-                    operand1 = operand2;
+                    operand1 = value;
                     break;
                 case "/":
-                    if (operand2 == 0){
+                    if (value == 0) {
                         operand1 = 0.0;
                     } else {
-                        operand1 /= operand2;
+                        operand1 /= value;
                     }
                     break;
                 case "*":
-                    operand1 *= operand2;
+                    operand1 *= value;
                     break;
                 case "-":
-                    operand1 -= operand2;
+                    operand1 -= value;
                     break;
                 case "+":
-                    operand1 += operand2;
+                    operand1 += value;
                     break;
             }
         }
